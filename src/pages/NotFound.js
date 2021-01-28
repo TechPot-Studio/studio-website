93cfdc88b31ec5cfa6b1b2c17813004386f8e590
TheColdPot <guohw979@outlook.com>
2021-01-28 13:47:37 +0800

    Main: Add 404 page

---
+++ src/pages/NotFound.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Button} from 'rsuite';
+import {Link} from 'react-router-dom';
+export default class NotFoundPage extends React.Component {
+    render() {
+        return (
+            <div style={{
+                width: '100%',
+                height: '500px',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                flexDirection: 'column'
+            }}>
+                <h6 style={{fontSize: 75, fontWeight: '600', lineHeight: '90px', textShadow: '5px 5px 5px #ccc'}}>404</h6>
+                <div style={{fontSize: 25}}>
+                    We cannot find the page you're looking for.
+                </div>
+                <br />
+                <br />
+                <Link to="/"><Button>HomePage</Button></Link>
+            </div>
+        );
+    }
+}
